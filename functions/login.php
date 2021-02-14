92d470749217ef77e4345bac561528d99cd67b07
Alfie Mills <alfie.mills@outlook.com>
2021-02-14 23:20:01 +0000

    Cool new wp-login theme

--- functions/login.php
+++ functions/login.php
@@ -1,7 +1,7 @@
 <?php
 // Calling your own login css so you can style it
 function joints_login_css() {
-	wp_enqueue_style( 'joints_login_css', get_template_directory_uri() . '/assets/styles/login.css', false );
+	wp_enqueue_style( 'joints_login_css', get_template_directory_uri() . '/assets/styles/login.css', true );
 }
 
 // changing the logo link from wordpress.org to your site
@@ -13,4 +13,17 @@ function joints_login_title() { return get_option('blogname'); }
 // calling it only on the login page
 add_action( 'login_enqueue_scripts', 'joints_login_css', 10 );
 add_filter('login_headerurl', 'joints_login_url');
-add_filter('login_headertitle', 'joints_login_title');
+add_filter('login_headertext', 'joints_login_title');
+
+
+?>
+
+<div class='ripple-background'>
+  <div class='circle xxlarge shade1'></div>
+  <div class='circle xlarge shade2'></div>
+  <div class='circle large shade3'></div>
+  <div class='circle mediun shade4'></div>
+  <div class='circle small shade5'></div>
+</div>
+
+<?php
